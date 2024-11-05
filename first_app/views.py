cc79e04e1466da117bc8afba84df74d1e6a47268
Shohagh019 <shohaghmunna@gmail.com>
2024-11-05 17:33:21 +0600

    select button added

--- first_app/views.py
+++ first_app/views.py
@@ -63,6 +63,8 @@ def about(request):
     if request.method == 'POST':
         name = request.POST.get('username')
         email = request.POST.get('email')
-        return render(request, './first_app/about.html', {'name': name, 'email': email})
+        select = request.POST.get('select')
+
+        return render(request, './first_app/about.html', {'name': name, 'email': email, 'select': select})
     else:
-        return render(request, './first_app/about.html', {'name': name, 'email': email})
+        return render(request, './first_app/about.html')
