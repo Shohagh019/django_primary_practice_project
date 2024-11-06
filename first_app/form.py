ff0e5ea89eacffe161dec7a2e63c7e8419b15f5d
Shohagh019 <shohaghmunna@gmail.com>
2024-11-06 17:39:54 +0600

    built-in validators and custom validators added

--- first_app/form.py
+++ first_app/form.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core import validators
 class LoginForm(forms.Form):
     name = forms.CharField(label = 'Full Name: ', help_text='Please Enter Your First & Last Name', required=False, widget=forms.Textarea(attrs={'id':'text_area', 'class':'class-1', 'placeholder':'Please Enter Your Full Name'}))
     email = forms.EmailField(label='User Email')
@@ -14,15 +14,27 @@ class LoginForm(forms.Form):
     All_above_info_is_valid = forms.BooleanField()
 
 
-class validForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput)
-    email = forms.CharField(widget=forms.EmailInput)
-    def clean(self):
-        cleaned_data = super().clean()
-        name_val = self.cleaned_data['name']
-        email_val= self.cleaned_data['email']
-        if len(name_val) <10:
-            raise forms.ValidationError('Enter a name with at least 10 characters!')
+# class validForm(forms.Form):
+#     name = forms.CharField(widget=forms.TextInput)
+#     email = forms.CharField(widget=forms.EmailInput)
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         name_val = self.cleaned_data['name']
+#         email_val= self.cleaned_data['email']
+#         if len(name_val) <10:
+#             raise forms.ValidationError('Enter a name with at least 10 characters!')
 
-        if '.com' not in email_val:
-            raise forms.ValidationError('Email must contain ".com"')
+#         if '.com' not in email_val:
+#             raise forms.ValidationError('Email must contain ".com"')
+
+def len_check(value):
+    if len(value) <10:
+        raise forms.ValidationError('Enter a value at least 10 chars')
+
+class validForm(forms.Form):
+    name = forms.CharField(validators=[validators.MinLengthValidator(10, message='')])
+    text = forms.CharField(widget=forms.TextInput, validators=[len_check])
+    email = forms.CharField(widget=forms.EmailInput, validators=[validators.EmailValidator(message='Enter a valid Email')])
+    age = forms.IntegerField(validators=[validators.MaxValueValidator(30, message='Age Must Maximum 30'), validators.MinValueValidator(18, message='Age must minimum 18')])
+    file = forms.FileField(validators=[validators.FileExtensionValidator(allowed_extensions=['pdf'], message='File must be in pdf format!')])
+
